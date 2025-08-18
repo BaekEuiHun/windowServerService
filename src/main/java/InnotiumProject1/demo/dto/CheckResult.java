1fcfea8807a9bf03695fcb577ed30a777534c28a
e.h.baek <euihunbaek@gmail.com>
2025-08-18 14:38:55 +0900

    개별 체크 항목 dto 생성

---
+++ src/main/java/InnotiumProject1/demo/dto/CheckResult.java
@@ -0,0 +1,9 @@
+package InnotiumProject1.demo.dto;
+
+
+//record : 불변 데이터 홀더 -> 자동으로 getter, equals, tostring 생성
+public record CheckResult(String checkFactorName, //점검 항목 이름(포트, dns, ping
+                          boolean ok, // 성공.실패
+                          String message, //부가 설명
+                          Long ms) { //소요시간
+}
