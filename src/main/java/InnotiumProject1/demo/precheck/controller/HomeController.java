fa32e7081b186b330ffb0a115011cd26d448699d
e.h.baek <euihunbaek@gmail.com>
2025-08-19 10:41:43 +0900

    메인화면 패키지 위치 변경

--- src/main/java/InnotiumProject1/demo/precheck/controller/HomeController.java
+++ src/main/java/InnotiumProject1/demo/precheck/controller/HomeController.java
@@ -1,4 +1,4 @@
-package InnotiumProject1.demo;
+package InnotiumProject1.demo.precheck.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
